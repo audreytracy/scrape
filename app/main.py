e61aab0efd49029b8e74029370eb42e8b4c33db0
unknown <audtracy@outlook.com>
2024-11-25 20:31:39 -0600

    remove secret

--- app/main.py
+++ app/main.py
@@ -1,6 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, BackgroundTasks
 from fastapi.responses import HTMLResponse
-
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver import ActionChains
+import time
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
 from dbos import DBOS
 
 # Welcome to DBOS!
@@ -25,31 +30,80 @@ def world_step() -> str:
     return "world"
 
 
-@app.get("/hello")
-@DBOS.workflow()
-def hello_world() -> str:
-    hello = hello_step()
-    world = world_step()
-    return f"{hello}, {world}!"
+# @app.get("/hello")
+# @DBOS.workflow()
+# def hello_world() -> str:
+#     hello = hello_step()
+#     world = world_step()
+#     return f"{hello}, {world}!"
 
 
 # This code uses FastAPI to serve an HTML + CSS readme from the root path.
 
 
+def send_email():
+    message = Mail(
+        from_email='audtracy@outlook.com',
+        to_emails='audtracy@gmail.com',
+        subject='ALERT: PCT PERMIT AVAILABLE IN MAY',
+        html_content='<strong>GO GET THE PERMIT JOSH. GO. PERMIT. NOW.</strong>')
+    try:
+        sg = SendGridAPIClient(api_key="")
+        response = sg.send(message)
+        print(response.status_code)
+        print(response.body)
+        print(response.headers)
+    except Exception as e:
+        print(e)
+
+def scrape():
+
+    element_list = []
+
+    page_url = "https://portal.permit.pcta.org/availability/mexican-border.php"
+
+    driver = webdriver.Chrome()
+    driver.get(page_url)
+
+    next_button = driver.find_elements(By.CLASS_NAME, "fc-icon.fc-icon-chevron-right")
+    title = driver.find_elements(By.CLASS_NAME, "fc-left")
+
+    ActionChains(driver).click(next_button[0]).perform() # into April
+    ActionChains(driver).click(next_button[0]).perform() # into May
+
+    while(True):
+
+        permit_days = driver.find_elements(By.CLASS_NAME, "fc-event-container")
+
+        for i in range(len(permit_days)):
+            print("----------------")
+            print(permit_days[i].text, permit_days[i].text.strip() != '35')
+            print("----------------")
+
+            if permit_days[i].text.strip() != '35':
+                # send_email()
+                print("HELLOOOO")
+                # break
+
+        time.sleep(1) # sleep 1 second
+
+    driver.close() # close webdriver
+
 @app.get("/")
 def readme() -> HTMLResponse:
+    scrape()
     readme = """
         <!DOCTYPE html>
         <html lang="en">
         <head>
-            <title>Welcome to DBOS!</title>
+            <title>Scrape</title>
             <link rel="icon" href="https://dbos-blog-posts.s3.us-west-1.amazonaws.com/live-demo/favicon.ico" type="image/x-icon">
             <script src="https://cdn.tailwindcss.com"></script>
         </head>
         <body class="font-sans text-gray-800 p-6 max-w-2xl mx-auto">
             <h1 class="text-xl font-semibold mb-4">Welcome to DBOS!</h1>
             <p class="mb-4">
-                This is a template built with DBOS and FastAPI. Visit <code class="bg-gray-100 px-1 rounded"><a href="/hello" class="text-blue-600 hover:underline">/hello</a></code> to see a "Hello, World!" message.
+                Yo
             </p>
             <p class="mb-4">
                 To get started building, edit <code class="bg-gray-100 px-1 rounded">app/main.py</code>, then visit the <a href="https://console.dbos.dev/applications" class="text-blue-600 hover:underline">cloud console</a> to redeploy your app.
